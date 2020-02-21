2d53fe8d025a530c75a148fa85d12fe30249ce16
dentaaa <61283005+dentaaa@users.noreply.github.com>
2020-02-21 16:33:49 +0700

    Update Moudl1_3_C07.sh

--- soal3/Moudl1_3_C07.sh
+++ soal3/Moudl1_3_C07.sh
@@ -2,7 +2,5 @@
 b=28
 for ((a=1; a <= $b; a=a+1))
 do
-wget https://loremflickr.com/320/240/cat -O dkt_kusuma_NO_$a.jpg -a wget.log 
+wget a wget.log  https://loremflickr.com/320/240/cat -O dkt_kusuma_NO_$a.jpg
 done
-
-#echo "hello world"
