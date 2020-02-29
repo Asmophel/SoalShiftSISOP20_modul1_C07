ffd6a04b7615c7d4287b602997a7e495de4e1351
dentaaa <61283005+dentaaa@users.noreply.github.com>
2020-02-29 18:02:53 +0700

    Update soal2_enkripsi.sh

--- soal2/soal2_enkripsi.sh
+++ soal2/soal2_enkripsi.sh
@@ -9,7 +9,7 @@ geser=$(stat -c %y $1 | grep -o -P '(?<= ).*(?=:.*:)')
 kecil="abcdefghijklmnopqrstuvwxyz"
 besar="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-caesar=$(echo "$filename" | sed "y/$kecil$besar/${kecil:$geser}${kecil::$geser}$
+caesar=$(echo "$filename" | sed "y/$kecil$besar/${kecil:$geser}${kecil::$geser}${besar:$geser}${besar::$geser}/")
 #echo $caesar
 
 mv $name "${caesar}".txt
